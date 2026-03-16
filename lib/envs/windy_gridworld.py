2a4dfa89f5cf688103c961267a140510c15b4723
Manisha <manishaggpb96@gmail.com>
2026-03-16 09:05:05 +0530

    Updated Windy Gridworld implementation

--- lib/envs/windy_gridworld.py
+++ lib/envs/windy_gridworld.py
@@ -1,14 +1,14 @@
-import gym
 import numpy as np
 import sys
-from gym.envs.toy_text import discrete
+from io import StringIO
 
 UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-class WindyGridworldEnv(discrete.DiscreteEnv):
+
+class WindyGridworldEnv:
 
     metadata = {'render.modes': ['human', 'ansi']}
 
@@ -19,61 +19,107 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
         coord[1] = max(coord[1], 0)
         return coord
 
+
     def _calculate_transition_prob(self, current, delta, winds):
-        new_position = np.array(current) + np.array(delta) + np.array([-1, 0]) * winds[tuple(current)]
+
+        new_position = np.array(current) + np.array(delta) + np.array([-1,0]) * winds[tuple(current)]
+
         new_position = self._limit_coordinates(new_position).astype(int)
+
         new_state = np.ravel_multi_index(tuple(new_position), self.shape)
-        is_done = tuple(new_position) == (3, 7)
+
+        is_done = tuple(new_position) == (3,7)
+
         return [(1.0, new_state, -1.0, is_done)]
 
+
     def __init__(self):
-        self.shape = (7, 10)
+
+        self.shape = (7,10)
 
         nS = np.prod(self.shape)
         nA = 4
 
-        # Wind strength
         winds = np.zeros(self.shape)
         winds[:,[3,4,5,8]] = 1
         winds[:,[6,7]] = 2
 
-        # Calculate transition probabilities
         P = {}
+
         for s in range(nS):
+
             position = np.unravel_index(s, self.shape)
-            P[s] = { a : [] for a in range(nA) }
-            P[s][UP] = self._calculate_transition_prob(position, [-1, 0], winds)
-            P[s][RIGHT] = self._calculate_transition_prob(position, [0, 1], winds)
-            P[s][DOWN] = self._calculate_transition_prob(position, [1, 0], winds)
-            P[s][LEFT] = self._calculate_transition_prob(position, [0, -1], winds)
 
-        # We always start in state (3, 0)
+            P[s] = {a:[] for a in range(nA)}
+
+            P[s][UP] = self._calculate_transition_prob(position,[-1,0],winds)
+            P[s][RIGHT] = self._calculate_transition_prob(position,[0,1],winds)
+            P[s][DOWN] = self._calculate_transition_prob(position,[1,0],winds)
+            P[s][LEFT] = self._calculate_transition_prob(position,[0,-1],winds)
+
+
         isd = np.zeros(nS)
-        isd[np.ravel_multi_index((3,0), self.shape)] = 1.0
+        isd[np.ravel_multi_index((3,0),self.shape)] = 1.0
 
-        super(WindyGridworldEnv, self).__init__(nS, nA, P, isd)
 
-    def _render(self, mode='human', close=False):
+        self.nS = nS
+        self.nA = nA
+        self.P = P
+        self.isd = isd
+
+        self.s = np.random.choice(range(nS),p=isd)
+
+
+    def reset(self):
+
+        self.s = np.random.choice(range(self.nS),p=self.isd)
+
+        return self.s,{}
+
+
+    def step(self,action):
+
+        transitions = self.P[self.s][action]
+
+        prob,next_state,reward,done = transitions[0]
+
+        self.s = next_state
+
+        return next_state,reward,done,False,{}
+
+
+    def render(self):
+
+        self._render()
+
+
+    def _render(self,mode='human',close=False):
+
         if close:
             return
 
-        outfile = StringIO() if mode == 'ansi' else sys.stdout
+        outfile = StringIO() if mode=='ansi' else sys.stdout
 
         for s in range(self.nS):
-            position = np.unravel_index(s, self.shape)
-            # print(self.s)
+
+            position = np.unravel_index(s,self.shape)
+
             if self.s == s:
                 output = " x "
+
             elif position == (3,7):
                 output = " T "
+
             else:
                 output = " o "
 
             if position[1] == 0:
                 output = output.lstrip()
-            if position[1] == self.shape[1] - 1:
+
+            if position[1] == self.shape[1]-1:
                 output = output.rstrip()
                 output += "\n"
 
             outfile.write(output)
+
         outfile.write("\n")
